8f06e563daee5bf25c09137f6dfb616cc1e22592
Lpilonieta <lpilonieta@unal.edu.co>
2022-11-05 14:57:25 -0500

    Empezando el visualModel, panel de logIn empezado,visualmente. MainFrame creado pero vacío.

---
+++ src/ViewModel/LogInFrame.java
@@ -0,0 +1,14 @@
+package ViewModel;
+
+
+import javax.swing.*;
+
+public class LogInFrame {
+
+    private JPanel principal;
+    private JTextField usernameInputField;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JPasswordField passwordInputField;
+    private JButton logInButton;
+}
